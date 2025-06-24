36bf99b34ecfd4380273ae4d3277441840a7b6be
henrrycoronado <caceresvillcamiguelangel@gmail.com>
2025-06-24 11:53:33 -0400

    add views complete

--- Frontend_vanilla/src/blocks/menu/menu.js
+++ Frontend_vanilla/src/blocks/menu/menu.js
@@ -4,7 +4,18 @@ export class Menu extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.addEventListener('click', event => {
+            const link = event.target.closest('a');
+            if (link) {
+                const href = link.getAttribute("href");
+                if (href && href.startsWith('/')) {
+                    event.preventDefault();
+                    app.router.go(href); 
+                }
+            }
+        });
     }
+    
 }
 
 customElements.define("menu-page", Menu);
--- Frontend_vanilla/src/blocks/notFound/notFound.js
+++ Frontend_vanilla/src/blocks/notFound/notFound.js
@@ -4,6 +4,16 @@ export class NotFound extends HTMLElement {
         super();
         this.attachShadow({ mode: 'open' });
         this.shadowRoot.appendChild(template.content.cloneNode(true));
+        this.shadowRoot.addEventListener('click', event => {
+            const link = event.target.closest('a');
+            if (link) {
+                const href = link.getAttribute("href");
+                if (href && href.startsWith('/')) {
+                    event.preventDefault();
+                    app.router.go(href); 
+                }
+            }
+        });
     }
 }
 
