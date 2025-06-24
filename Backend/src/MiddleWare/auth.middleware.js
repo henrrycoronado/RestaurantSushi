b2cb2083ab274d4646b6a7a5df16a3b5d3ffb0b9
henrrycoronado <caceresvillcamiguelangel@gmail.com>
2025-06-24 00:21:41 -0400

    add auth detail

--- Backend/src/MiddleWare/auth.middleware.js
+++ Backend/src/MiddleWare/auth.middleware.js
@@ -5,7 +5,7 @@ export const protect = async (req, res, next) => {
     let token;
     if (
         req.headers.authorization &&
-        req.headers.authorization.startsWith('Bearer')
+        req.headers.authorization.startsWith('Hc')
     ) {
         try {
             token = req.headers.authorization.split(' ')[1];
